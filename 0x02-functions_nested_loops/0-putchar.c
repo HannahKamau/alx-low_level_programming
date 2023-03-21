85ae46901fac78211c95b97abd1900a6bbebfa26
HannahKamau <hannahkamau208@gmail.com>
2023-03-21 13:19:10 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
-* main - print _putchar
-* is written by mbah
-* Return: 0
-*/
-
+ * main - print _putchar
+ * is writen ny mbah
+ * Return: 0
+ */
 
 int main(void)
 {
@@ -18,4 +17,4 @@ _putchar('a');
 _putchar('r');
 _putchar('\n');
 return (0);
-}  
+}
