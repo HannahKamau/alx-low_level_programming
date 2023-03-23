d3111194a788c3563efaba53181686ed0e584c63
HannahKamau <hannahkamau208@gmail.com>
2023-03-23 12:45:19 -0700

    My files

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -13,19 +13,4 @@ int mul;
 mul = a * b;
 
 return (mul);
-}#include "main.h"
-
-/**
- *  *  mul - multiplies two integers
- *   *  @a:a - Variable
- *    *  @b:b - Variable
- *     *  Return:  Always 0 success
- *      */
-int mul(int a, int b)
-{
-	int mul;
-
-	mul = a * b;
-
-	return (mul);
 }
